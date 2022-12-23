c80b97d4b41e8d5cd057d3b1fddfe0e666889c73
Bryn Dole <bryn.dole@gmail.com>
2022-12-23 15:26:57 -0800

    pigeon on canivore

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,9 @@ public final class Constants {
 
     public static final class Swerve {
         public static final int pigeonID = 15;
+        public static final String pigeonCANBusName = "CANivore";
+
+        public static final String motorsCANBusName = "";
 
         public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
